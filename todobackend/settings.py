ca826d3a293a91e35e635683e98c7beb74693d02
Deepanshu Sharma <deepanshu2912001@gmail.com>
2023-07-13 22:32:15 +0530

    allow host for deploy

--- todobackend/settings.py
+++ todobackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t_-)pn_ivis@xbg43#%r6lhr0)n!^@n8q#-_lyyhj6k61j(lsc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
